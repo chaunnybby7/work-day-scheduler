4f6ae79168f5c38d454bd6cfae43c1692c5a1f96
YiLin Ong <chauntelleinc@gmail.com>
2022-04-09 18:23:45 -0400

    added contents to README.md and index.html

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # work-day-scheduler
+
+![Top Language](https://img.shields.io/github/languages/top/chaunnybby7/weather-dashboard)
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+Used the [Moment.js](https://momentjs.com/) library to work with date and time. 
+
+`localStorage` is used to store data. 
+
+## Installation
+
+1. Download or clone repository
+2. Open the main page (index.html) on your browser to view
+3. Use a text editor to view all coding. I'm using Visual Studio Code. 
+
+## Functionality
+
+- On the index.html page, user will see the current date and time. 
+  
+- The time slots are 9 hours range from 8AM to 5PM. 
+- The tasks/to-do list text area are divided into 3 different colors: 
+* Past: ![#d3d3d3](https://via.placeholder.com/15/d3d3d3/000000?text=+)  Grey
+* Present: ![#ff6961](https://via.placeholder.com/15/ff6961/000000?text=+) Red
+* Future: ![#77dd77](https://via.placeholder.com/15/77dd77/000000?text=+) Green
+
+  
+## Features
+
+* HTML
+* CSS
+* Bootstrap
+* jQuery
+* Moment.js
+* Font Awesome Icon 
+
+## Demo
+
+![Work Day Scheduler Demo]()
+
+## Websites
+
+* [Github](https://github.com/chaunnybby7/weather-dashboard)
+* [Deployed](https://chaunnybby7.github.io/weather-dashboard/)
+
+## License
+
+  Copyright (c) YiLin Ong. All rights reserved.
+  
+  Licensed under the [MIT](LICENSE) license.
---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// The ready() method is used to make a function available after the document is loaded. 
+//Whatever code you write inside the $(document ).ready() method will run once the page DOM is ready to execute JavaScript code.
+$(document).ready(function () {
+// Display current day & time.
+
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+
+//assign saveBtn click eventlistener for user input and time stamp
+$(".saveBtn").on("click", function () {
+//get nearby values.
+console.log(this);
+var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+//set items in local storage.
+localStorage.setItem(time, text);
+    })
+
+//load any saved data from LocalStorage - do this for each hour created. 
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+//get current number of hours.
+function hourTracker() {
+
+var currentHour = moment().hour(); // use of moment.js
+
+// loop over time blocks
+$(".time-block").each(function () {
+var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+//check if we've moved past this time, click into css/html given classes of past, present, or future
+if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+ } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker(); //run function again 
+})
